aa8d0c28e774e93ba76ba40af56c0d3bf200ab4d
Jay Bhatt <jayb96114@gmail.com>
2026-03-21 16:54:50 +0530

    Add files via upload

--- src/entities/all.js
+++ src/entities/all.js
@@ -1,5 +1,14 @@
 import { base44 } from '@/api/base44Client';
 
-export const Quiz = base44.entities.Quiz;
-export const QuizAttempt = base44.entities.QuizAttempt;
+const createEntity = (name) => ({
+  list: (...args) => base44.entities[name].list(...args),
+  get: (...args) => base44.entities[name].get(...args),
+  create: (data) => base44.entities[name].create(data),
+  update: (id, data) => base44.entities[name].update(id, data),
+  delete: (id) => base44.entities[name].delete(id),
+  filter: (...args) => base44.entities[name].filter(...args),
+});
+
+export const Quiz = createEntity("Quiz");
+export const QuizAttempt = createEntity("QuizAttempt");
 export const User = base44.auth;
