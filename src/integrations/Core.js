7260784d782994bd80afdc8fb0a27d834b22a7af
Jay Bhatt <jayb96114@gmail.com>
2026-03-22 08:35:55 +0530

    Add files via upload

--- src/integrations/Core.js
+++ src/integrations/Core.js
@@ -1,21 +1,21 @@
 import { base44 } from '@/api/base44Client';
 
 export const InvokeLLM = async ({ prompt, response_json_schema }) => {
-  return await base44.functions.InvokeLLM({ prompt, response_json_schema });
+  return await base44.functions.invoke("InvokeLLM", { prompt, response_json_schema });
 };
 
 export const SendEmail = async (data) => {
-  return await base44.functions.SendEmail(data);
+  return await base44.functions.invoke("SendEmail", data);
 };
 
 export const UploadFile = async (data) => {
-  return await base44.functions.UploadFile(data);
+  return await base44.functions.invoke("UploadFile", data);
 };
 
 export const GenerateImage = async (data) => {
-  return await base44.functions.GenerateImage(data);
+  return await base44.functions.invoke("GenerateImage", data);
 };
 
 export const ExtractDataFromUploadedFile = async (data) => {
-  return await base44.functions.ExtractDataFromUploadedFile(data);
+  return await base44.functions.invoke("ExtractDataFromUploadedFile", data);
 };
